6076c19ff56ccdc2c619261b39ab48211bbaa99b
Aidah Novallia Putri <aidahptr15@gmail.com>
2025-02-16 21:55:22 +0700

    feat: add new level

--- scenes/ObjectiveArea.gd
+++ scenes/ObjectiveArea.gd
@@ -2,4 +2,10 @@ extends Area2D
 
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 	if (body.name == "BlueShip"):
-		print("Reached objective!")
+		print("Reached objective Level 1!")
+		call_deferred("change_scene", "res://scenes/MainLevel2.tscn")
+	elif (body.name == "GreenShip"):
+		print("Reached objective Level 2!")
+
+func change_scene(scene_path: String):
+	get_tree().change_scene_to_file(scene_path)
--- scenes/PlatformWhite.gd
+++ scenes/PlatformWhite.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Platform Level 1 initialized")
+	print("Platform Level 2 initialized")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,7 +13,7 @@ func _process(delta):
 	
 	if Input.is_action_pressed("move_down"):
 		move_local_y(speed * delta, false)
-	
+		
 	if Input.is_action_pressed("move_right"):
 		move_local_x(speed * delta, false)
 		
